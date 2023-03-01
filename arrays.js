b1339fe4e346813224c4024d32b8ac550b3219f5
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-01 13:05:16 -0800

    Add an element to boards array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
 var boards = ["snow", "skate", "surf", "skim"];
 var length = [3, 4, 5, 6];
 var bool = [true, true, false, false];
+
+// Add an element to the end of the array. 
+boards.push("long")
+console.log(boards)
+// Will return ['snow', 'skate', 'surf', 'skim', 'long']
