d52ab51440bc0bb301d65ffbd0793ecc244755f8
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-01 13:12:21 -0800

    Demonstrate knowledge of index positions

--- arrays.js
+++ arrays.js
@@ -3,6 +3,21 @@ var length = [3, 4, 5, 6];
 var bool = [true, true, false, false];
 
 // Add an element to the end of the array. 
-boards.push("long")
-console.log(boards)
-// Will return ['snow', 'skate', 'surf', 'skim', 'long']
+boards.push("long");
+console.log(boards);
+// Expected output: ['snow', 'skate', 'surf', 'skim', 'long']
+
+// Remove last element and return.
+length.pop();
+console.log(length);
+// Expected output: [3, 4, 5]
+
+// Remove the first element and return.
+console.log(bool.shift());
+// Expected output: true. 
+
+console.log(bool);
+// Expected output: [true, false, false]
+
+console.log(boards[2])
+// Expected output: surf
